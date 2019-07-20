bb52d283c41e3cf8073b853f89352fe3f62623c4
Akshay Mahajan <akshayym94@gmail.com>
2019-07-20 14:07:18 +0530

    Initial Commit

--- example/src/App.js
+++ example/src/App.js
@@ -1,13 +1,92 @@
-import React, { Component } from 'react'
-
-import ExampleComponent from 'react-click-copy'
+import React, { Component } from "react";
+import ClickToCopy from "react-click-copy";
 
 export default class App extends Component {
-  render () {
+  state = {
+    customSuccesss: ""
+  };
+  componentDidMount() {
+    this.mounted = true;
+  }
+
+  onCopy = (type, msg) => {
+    if (this.mounted) {
+      this.setState(
+        {
+          [type]: msg
+        },
+        () => {
+          setTimeout(() => {
+            this.setState({
+              [type]: ""
+            });
+          }, 3000);
+        }
+      );
+    }
+  };
+
+  render() {
     return (
-      <div>
-        <ExampleComponent text='Modern React component module' />
+      <div className="container react-click-copy">
+        <h1 className="text-center">react-click-copy</h1>
+        <p className="text-center">
+          A Fully Customizable Click-To-Copy Component for React.
+          <br />
+          <a href="https://github.com/akshayymahajan/react-click-copy">
+            Checkout the Documentation on Github
+          </a>
+        </p>
+
+        <section className="text-center">
+          <pre>{`
+            <ClickToCopy
+              contentToCopy="This is the default look and feel"
+            />
+          `}</pre>
+          <ClickToCopy contentToCopy="This is the default look and feel" />
+          <p>{this.state.defaultSuccess}</p>
+        </section>
+
+        <section className="text-center">
+          <pre>{`
+            <ClickToCopy
+              contentToCopy="You can use custom JSX element for the button"
+              render={props => (
+                <a href="#copy" onClick={props.copy}>
+                  Copy
+                </a>
+              )}
+            />
+          `}</pre>
+          <ClickToCopy
+            contentToCopy="You can use custom JSX element for the button"
+            render={props => (
+              <a href="#copy" onClick={props.copy}>
+                Copy
+              </a>
+            )}
+          />
+        </section>
+
+        <section className="text-center">
+          <pre>{`
+            <ClickToCopy
+              contentToCopy="You can even set a custom action to be triggered on copy"
+              render={props => (
+                <a href="#copy" onClick={props.copy}>
+                  Copy
+                </a>
+              )}
+            />
+          `}</pre>
+          <ClickToCopy
+            contentToCopy="You can even set a custom action to be triggered on copy"
+            onCopy={() => this.onCopy("customSuccess", "Copied Successfully!")}
+          />
+          <p>{this.state.customSuccess}</p>
+        </section>
       </div>
-    )
+    );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,22 +1,92 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-
 import styles from './styles.css'
 
-export default class ExampleComponent extends Component {
+export default class ClickToCopy extends Component {
+  constructor(props) {
+    super(props)
+    this.textAreaRef = React.createRef()
+    this.state = {
+      popoverVisible: false
+    }
+  }
+
   static propTypes = {
-    text: PropTypes.string
+    contentToCopy: PropTypes.string.isRequired
+  }
+
+  copyToClipboard = e => {
+    this.textAreaRef.current.select()
+    document.execCommand('copy')
+    if (this.props.onCopy) {
+      this.props.onCopy()
+    } else {
+      this.togglePopover()
+    }
+  }
+
+  togglePopover() {
+    if (this.state.popoverVisible) {
+      if (this.popoverTimeout) {
+        clearTimeout(this.popoverTimeout)
+      }
+      this.setState({
+        popoverVisible: false
+      })
+    } else {
+      this.setState(
+        {
+          popoverVisible: true
+        },
+        () => {
+          this.popoverTimeout = setTimeout(() => {
+            this.setState({
+              popoverVisible: false
+            })
+          }, 3000)
+        }
+      )
+    }
   }
 
   render() {
-    const {
-      text
-    } = this.props
-
     return (
-      <div className={styles.test}>
-        Example Component: {text}
-      </div>
+      <form className={styles.reactClickCopy}>
+        <div
+          style={{
+            display: this.state.popoverVisible ? 'block' : 'none'
+          }}
+          className={styles.popover}
+        >
+          Copied Successfully!
+        </div>
+        <div
+          className={styles.popoverBgOverlay}
+          style={{
+            visibility: this.state.popoverVisible ? 'visible' : 'hidden'
+          }}
+          onClick={() => this.togglePopover()}
+        />
+        <textarea
+          className={styles.textarea}
+          ref={this.textAreaRef}
+          value={this.props.contentToCopy}
+          rows={0}
+          cols={0}
+          readOnly
+        />
+        {this.props.render ? (
+          this.props.render({ copy: this.copyToClipboard })
+        ) : (
+          <button
+            type='button'
+            className={styles.btnDefault}
+            onClick={this.copyToClipboard}
+          >
+            Copy
+          </button>
+        )}
+      </form>
     )
   }
 }
